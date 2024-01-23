071ba4e950678689a8239511eefa745f1ec4d6db
vijayktech <Vijay.techgeek@gmail.com>
2024-01-23 16:06:07 +0530

    pushing the ts project

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+// src/index.ts
+function hello(name) {
+  console.log("Welcome to ESBuild !");
+  return "Hello " + name;
+}
+var greets = hello("vijay");
+console.log(greets);
+export {
+  hello
+};
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export function hello(name : string) : string {
+    console.log("Welcome to ESBuild !");
+    return 'Hello ' + name; 
+}
+let greets = hello('vijay');
+console.log(greets);
